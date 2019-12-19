8abcb2d1a8dde7c03778de61da55780700b1f1fd
Cariama <58600203+Cariama@users.noreply.github.com>
2019-12-19 14:23:13 +0100

    Update EVkinter.py

--- EVkinter.py
+++ EVkinter.py
@@ -2,10 +2,6 @@ from tkinter import *
 import time
 from EV import Evol
 import random
-
-def get_colour():
-    nn = int(random.random()*0xFFFFFF)
-    return "#"+(8-len(hex(nn)))*"0"+hex(nn)[2:]
     
 my_random_colours =[get_colour() for i in range(1000)]
 #print(random_colours)
@@ -28,7 +24,7 @@ class display :
         window2 = Tk()
         canvas = Canvas(window2, width=(self.class_in.x-1)*100*self.scale,height = (self.class_in.y-1)*100*self.scale, background = "white")
         for i in self.class_in.creatures :
-            canvas.create_oval((i.position[0]-0.5)*100*self.scale,(i.position[1]-0.5)*100*self.scale,(i.position[0]+0.5)*100*self.scale,(i.position[1]+0.5)*100*self.scale,fill = "#555555")
+            canvas.create_oval((i.position[0]-i.hitbox)*100*self.scale,(i.position[1]-i.hitbox)*100*self.scale,(i.position[0]+i.hitbox)*100*self.scale,(i.position[1]+i.hitbox)*100*self.scale,fill = "#555555")
         canvas.create_text(200, 60, text="press s to start the simulation", font="Arial 16 italic", fill="black")
         canvas.pack()
         def clavier(event):
@@ -40,11 +36,11 @@ class display :
                         canvas.delete(ALL)
                         self.class_in.move()
                         for index,i in enumerate(self.class_in.creatures) :
-                            canvas.create_oval( (i.position[0]-0.5)*100*self.scale, (i.position[1]-0.5)*100*self.scale, (i.position[0]+0.5)*100*self.scale, (i.position[1]+0.5)*100*self.scale,fill = my_random_colours[index])
+                            canvas.create_oval( (i.position[0]-i.hitbox)*100*self.scale, (i.position[1]-i.hitbox)*100*self.scale, (i.position[0]+i.hitbox)*100*self.scale, (i.position[1]+i.hitbox)*100*self.scale,fill = i.colour)
                         for j in self.class_in.foods :
                             canvas.create_oval( (j[0]-0.125)*100*self.scale, (j[1]-0.125)*100*self.scale, (j[0]+0.125)*100*self.scale, (j[1]+0.125)*100*self.scale,fill = "#005500")
-                        time.sleep(0)
-                        canvas.create_text(50, 20, text="hour : {}".format(self.class_in.t), font="Arial 16 italic", fill="black")
+                        time.sleep(0.05)
+                        canvas.create_text(50, 20, text="hour : {}\nday : {}".format(self.class_in.t, self.class_in.day), font="Arial 16 italic", fill="black")
                         canvas.update()
         canvas.focus_set()
         canvas.bind("<Key>", clavier)
