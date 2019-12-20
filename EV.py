a0ffacff163c3cb6deccf38081b93cb28ea49a34
Cariama <58600203+Cariama@users.noreply.github.com>
2019-12-20 17:18:42 +0100

    Update EV.py

--- EV.py
+++ EV.py
@@ -1,7 +1,8 @@
 import numpy as np
 import random as rd
 
-def conso(speed, hitbox, sence) :
+def conso(speed, hitbox, sence) : 
+    '''function par défaut régulant la consommation'''
     value = 0.25*speed*np.pi*hitbox**2*np.sqrt(sence)
     return value
 
@@ -9,26 +10,26 @@ class Creature:
     def __init__(self, position, angle, base_speed,base_energie, hitbox, sence,  generation=0, comportement=0,var1=1, var2=1, var3=1, function = conso):
         self.position = position
         self.angle = angle + Creature.add_angle()
-        if var1 :
+        if var1 : # = 1 si on effectue les mutation de vitesse
             self.speed = Creature.abs_rd_gauss(base_speed, 0.3)
         else :
             self.speed = 1
-        if var2 :
+        if var2 :# = 1 si on effectue les mutation de hitbox(rayon de la hitbox)
             self.hitbox = Creature.abs_rd_gauss(hitbox, 0.12)
             if self.hitbox < 0.05 :
                 self.hitbox = 0.05
         else :
             self.hitbox=0.5
         self.energie = base_energie
-        if var3 :
+        if var3 :# = 1 si on effectue les mutation de sence(rayon de vision)
             self.sence = Creature.abs_rd_gauss(sence, 0.12)
             if self.sence < 0.05 :
                 self.sence = 0.05
         else :
             self.sence=1
-        self.comp = comportement
+        self.comp = comportement # 0 pour deplacement normal, 1 pour retourner à la maison et 2 pour rester à la maison
         self.gen = generation
-        ratio = 255/(self.speed + self.hitbox + self.sence)
+        ratio = 255/(self.speed + self.hitbox + self.sence) # pour color
         self.colour = Creature.rgb_to_hex((int(self.speed * ratio),int(self.hitbox * ratio), int(self.sence * ratio)))
         self.down_energy = function
         
@@ -45,13 +46,15 @@ class Creature:
         else:
             return(0)
     
-    def abs_rd_gauss(base_speed, sigma):
-        return abs(base_speed + rd.gauss(0, sigma))
+    def abs_rd_gauss(base, sigma):
+        '''Ajoute  une valeur aléatoir venant d'une gaussienne de centre 0 et d'écart type sigma, puis donne sa valeur absolue'''
+        return abs(base + rd.gauss(0, sigma))
         
     def dist(self,coo):
         return np.sqrt(np.sum((self.position - coo) ** 2))
     
-    def anti_exit(self, x_max, y_max):#! mettre les x/y max des coordonnées
+    def anti_exit(self, x_max, y_max):
+        '''Empêche la créatures de sortir du plateau'''
         if self.position[0] < 0:
             self.position[0] = 0
             self.angle = 0 + Creature.add_angle()
@@ -69,6 +72,7 @@ class Creature:
             self.angle = -np.pi * 0.5 + Creature.add_angle()
             
     def nearest_food(self, foods, creatures, rp):
+        ''' Trouve la nourriture la plus proche et renvoit la distance de celle-ci et sa position'''
         d = 10**1000
         the_food=0
         for food in foods:
@@ -77,7 +81,7 @@ class Creature:
                 d = di
                 the_food = food
         for creature in creatures:
-            if creature.hitbox ** 2< self.hitbox ** 2 * rp:
+            if creature.hitbox ** 2 < self.hitbox ** 2 * rp:
                 di=Creature.dist(self, creature.position)
                 if di < d:
                     d = di
@@ -85,6 +89,7 @@ class Creature:
         return d, the_food
     
     def nearest_pred(self, creatures, rp):
+        ''' Trouve le prédateur l plus proche et renvoit la distance de celui-ci et sa position'''
         d = 10 ** 1000
         the_pred = 0
         for crea in creatures:
@@ -102,7 +107,7 @@ class Creature:
         except ZeroDivisionError:
             direction = 0
         vitesse = min(self.speed, Creature.dist(self, coo))
-        self.position += -direction * vitesse
+        self.position += - direction * vitesse
         self.energie -= self.down_energy(self.speed,self.hitbox,self.sence)
         
     def moveToward(self,coo):
@@ -130,7 +135,7 @@ class Creature:
             if self.comp == 0:
                 self.angle += Creature.add_angle()
                 
-    def go_home(self, x, y) : #! mettre les x/y max des coordonnées
+    def go_home(self, x, y) :
         dist_up = y  - self.position[1]
         dist_down = self.position[1]
         dist_right = x  - self.position[0]
@@ -146,7 +151,7 @@ class Evol:
     def __init__(self, n_creatures, x, y, rapport_nourriture=0.2, rapport_predation=0.42, t_day = 24, V1 = 1, V2 = 2,V3 = 3, func = conso):
         self.x, self.y = x, y
         self.rap_food = rapport_nourriture
-        self.food_hitbox=0.125
+        self.food_hitbox = 0.125
         self.rp=rapport_predation
         self.foods = Evol.foods_init(self)
         start_loc, start_angle = Evol.init_states(self, n_creatures)
@@ -154,12 +159,13 @@ class Evol:
         self.t = 0
         self.t_day = t_day
         self.day = 0
-        self.V1 = V1
-        self.V2 = V2
-        self.V3 = V3
-        self.func = func
+        self.V1 = V1 #gère l'activation de la variation de vitesse
+        self.V2 = V2 #gère l'activation de la variation de hitbox
+        self.V3 = V3 #gère l'activation de la variation de sence
+        self.func = func #function de consomation d'énergie
         
     def init_states(self, n_creatures):
+        '''Répartit les créatures sur les cotés de la carte, et leur donne leur angle de base'''
         oy,ox,my,mx=0,0,0,0 #oy:y=0, ox:x=0, my:y=max, mx:x=max
         for i in range(n_creatures):#distribue les creatures sur les cotés
             if i % 4 == 0:
@@ -173,26 +179,27 @@ class Evol:
                 
             elif (i - 1) % 2 == 0:
                 mx += 1
-
         oy_s = [np.array([rd.random() * self.x, 0]) for i in range(oy)]
         ox_s = [np.array([0, rd.random() * self.y]) for i in range(oy)]
-        my_s = [np.array([rd.random() * self.x, float(self.y-1)]) for i in range(oy)]
-        mx_s = [np.array([float(self.x-1), rd.random() * self.y]) for i in range(oy)]
+        my_s = [np.array([rd.random() * self.x, float(self.y )]) for i in range(oy)]
+        mx_s = [np.array([float(self.x), rd.random() * self.y]) for i in range(oy)]
         angles = [np.pi * 0.5 for i in range(oy)] + [0 for i in range(ox)] + [-np.pi * 0.5 for i in range(my)] + [np.pi for i in range(mx)]
         return oy_s + ox_s + my_s + mx_s, angles
     
-    def foods_init(self):#définit les emplacements de la nourritures
+    def foods_init(self):
+        '''Définit l'emplacement de la nourriture'''
         l=[]
         for xi in range(1, self.x-1):
-            for yi in range(1, self.y-1):
+            for yi in range(1, self.y - 1):
                 l.append(np.array([xi + rd.gauss(0, 0.35), yi + rd.gauss(0,0.35)]))
-        n_food = int((self.x-2) * (self.y-2) * self.rap_food + 0.5)
+        n_food = int((self.x-1) * (self.y - 1) * self.rap_food + 0.5)
         return rd.sample(l,n_food)
     
     def dup(self,crea):
+        '''Gère la reproduction'''
         a = np.array([rd.uniform(-0.1, 0.1), rd.uniform(-0.1, 0.1)])
         new_crea = Creature(crea.position + a, crea.angle, crea.speed, crea.energie/2, crea.hitbox, crea.sence, generation = crea.gen + 1, var1 = self.V1, var2 = self.V2, var3 = self.V3, function = self.func)
-        new_crea.anti_exit(self.x-1, self.y-1)
+        new_crea.anti_exit(self.x, self.y )
         crea.energie /= 2
         return new_crea
     
@@ -224,27 +231,28 @@ class Evol:
             del self.creatures[i]
     
     def kill(self) :
+        '''Tue les créatures n'étant pas à la maison'''
         lenght = len(self.creatures)
         for i, crea in enumerate(reversed(self.creatures)) :
-            if crea.position[0] < self.x-1.1 and crea.position[0] > 0.1 and crea.position[1] < self.y - 1.1 and crea.position[1] > 0 :
+            if crea.position[0] < self.x-0.1 and crea.position[0] > 0.1 and crea.position[1] < self.y - 0.1 and crea.position[1] > 0 :
                 del self.creatures[lenght - i - 1]
     
     def move(self):
         #Le temps défile
-        self.t+=1
+        self.t += 1
         #Deplcement
         for crea in self.creatures:
             crea.moving(self.foods, self.creatures, self.rp)
-            crea.anti_exit(self.x-1, self.y-1)
+            crea.anti_exit(self.x, self.y )
         #Se nourrir
         Evol.feed(self) 
         #Mourir de faim
         l=len(self.creatures)
         for i, crea in enumerate(reversed(self.creatures)):
-            if crea.energie<=0:
+            if crea.energie <= 0:
                 del self.creatures[l-i-1]
         #Fin de journée
-        if self.t>=self.t_day:
+        if self.t >= self.t_day:
             self.t = 0
             self.day += 1
             Evol.kill(self)
@@ -261,8 +269,8 @@ class Evol:
         #Changement de comportement
         for crea in self.creatures:
             if crea.comp == 0 and (crea.energie >= max([self.x, self.y]) * 0.25 * crea.speed + 25 * crea.hitbox ** 2 or self.t >= self.t_day*0.75):
-                crea.go_home(self.x-1,self.x-2)
+                crea.go_home(self.x,self.y)
                 crea.comp = 1
                 
-            if crea.comp == 1 and (crea.position[0] > self.x-1.1 or crea.position[0] < 0.1 or crea.position[1] > self.y - 1.1 or crea.position[1] < 0.1):
+            if crea.comp == 1 and (crea.position[0] > self.x - 0.1 or crea.position[0] < 0.1 or crea.position[1] > self.y - 0.1 or crea.position[1] < 0.1):
                 crea.comp = 2
